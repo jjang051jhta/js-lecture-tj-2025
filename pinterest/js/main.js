6120abc0fedcf9bcaba539110fdce4afd226297a
jjang051 <jjang051jhta@gmail.com>
2025-11-07 11:35:09 +0900

    pinterest

--- pinterest/js/main.js
+++ pinterest/js/main.js
@@ -1,5 +1,6 @@
 const itemList = document.querySelector(".itemList");
 const btns = document.querySelectorAll("#filter ul li");
+const menu = document.querySelector("#filter ul");
 console.log(btns);
 //유사 배열은 배열처러 보이지만 배열은 아니다.
 // 즉 배열의 여러 메서드들을 전부 사용할 수는 없다.
@@ -34,17 +35,32 @@ async function loadJson() {
     iso.layout();
   });
   console.log("iso===", iso);
-  btns.forEach((item) => {
-    item.addEventListener("click", () => {
-      //console.log("click");
-      console.log(item.dataset.filter);
-      const filterItem = "." + item.dataset.filter;
-      btns.forEach((btn) => {
-        btn.classList.remove("on");
-      });
-      item.classList.add("on");
-      iso.arrange({ filter: filterItem });
+  menu.innerHTML += '<li class="item www">WWW</li>';
+  //자바스크립트에서 동적으로 하나 더 추가
+  // btns.forEach((item) => {
+  //   item.addEventListener("click", () => {
+  //     //console.log("click");
+  //     console.log(item.dataset.filter);
+  //     const filterItem = "." + item.dataset.filter;
+  //     btns.forEach((btn) => {
+  //       btn.classList.remove("on");
+  //     });
+  //     item.classList.add("on");
+  //     iso.arrange({ filter: filterItem });
+  //   });
+  // });
+
+  menu.addEventListener("click", (e) => {
+    //console.log("click");
+    const item = e.target.closest("li");
+    if (!item) return;
+    const siblings = menu.querySelectorAll("li");
+    const filterItem = "." + item.dataset.filter;
+    siblings.forEach((btn) => {
+      btn.classList.remove("on");
     });
+    item.classList.add("on");
+    iso.arrange({ filter: filterItem });
   });
 }
 loadJson();
